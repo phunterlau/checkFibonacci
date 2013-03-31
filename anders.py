ae928aa3195ef423002f6cdc49e0dcb2473fbbaf
Hongliang Liu <phunter.lau@gmail.com>
2013-03-31 01:45:01 -0700

    Create anders.py

---
+++ anders.py
@@ -0,0 +1,19 @@
+import time
+
+def fibPlus((a,b),(c,d)):
+  bd = b*d
+	return (bd-(b-a)*(d-c), a*c+bd)
+
+def unFib((a,b),n):
+	if n<a: 
+		return (0,0,1)
+	else:
+		(k,c,d) = unFib(fibPlus((a,b),(a,b)),n)
+		(e,f) = fibPlus((a,b),(c,d))
+		if n<e: return (2*k, c, d)
+		else:
+			return (2*k+1,e,f)
+
+def isfib(n):
+	(k,a,b) = unFib((1,1),n)
+	return n==a
